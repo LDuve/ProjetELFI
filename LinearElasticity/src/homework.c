c2b93009d185dfd4c91466a665b00e4856a4b976
vtr4651 <victor.schyns@hotmail.com>
2023-05-17 13:21:43 +0200

    new

--- LinearElasticity/src/homework.c
+++ LinearElasticity/src/homework.c
@@ -170,25 +170,13 @@ void geoMeshGenerateGeoProjet() {
     gmshFltkRun(&ierr);
 }
 
-void geoMeshGenerateGeo() {
 
+
+void geoMeshGenerateBar() {
+    
     femGeo* theGeometry = geoGetGeometry();
     geoSetSizeCallback(geoSize);   
 
-
-    /*
-    4 ------------------ 3
-    |                    |
-    |                    |
-    5 ------- 6          |
-               \         |
-                )        |
-               /         |
-    8 ------- 7          |
-    |                    |
-    |                    |
-    1 ------------------ 2
-    */
 
     int ierr;
     double w = theGeometry->LxPlate;  // w=1
@@ -202,26 +190,27 @@ void geoMeshGenerateGeo() {
     int p2 = gmshModelGeoAddPoint( w/2, -h/2, 0., lc, 2, &ierr);
     int p3 = gmshModelGeoAddPoint( w/2,  h/2, 0., lc, 3, &ierr);
     int p4 = gmshModelGeoAddPoint(-w/2,  h/2, 0., lc, 4, &ierr);
-    int p5 = gmshModelGeoAddPoint(-w/2,    r, 0., lc, 5, &ierr);
-    int p6 = gmshModelGeoAddPoint(0.,      r, 0., lc, 6, &ierr);
-    int p7 = gmshModelGeoAddPoint(0.,     -r, 0., lc, 7, &ierr);
-    int p8 = gmshModelGeoAddPoint(-w/2,   -r, 0., lc, 8, &ierr);
-    int p9 = gmshModelGeoAddPoint(0.,     0., 0., lc, 9, &ierr); // center of circle
+    //int p5 = gmshModelGeoAddPoint(-w/2,    r, 0., lc, 5, &ierr);
+    //int p6 = gmshModelGeoAddPoint(0.,      r, 0., lc, 6, &ierr);
+    //int p7 = gmshModelGeoAddPoint(0.,     -r, 0., lc, 7, &ierr);
+    //int p8 = gmshModelGeoAddPoint(-w/2,   -r, 0., lc, 8, &ierr);
+    //int p9 = gmshModelGeoAddPoint(0.,     0., 0., lc, 9, &ierr); // center of circle
 
 
 
     int l1 = gmshModelGeoAddLine(p1, p2, 1, &ierr);
     int l2 = gmshModelGeoAddLine(p2, p3, 2, &ierr);
     int l3 = gmshModelGeoAddLine(p3, p4, 3, &ierr);
-    int l4 = gmshModelGeoAddLine(p4, p5, 4, &ierr);
-    int l5 = gmshModelGeoAddLine(p5, p6, 5, &ierr);
-    int l6 = gmshModelGeoAddCircleArc(p7, p9, p6, 6, 0., 0., 0., &ierr); // NB : the direction of the curve is reversed
-    int l7 = gmshModelGeoAddLine(p7, p8, 7, &ierr);
-    int l8 = gmshModelGeoAddLine(p8, p1, 8, &ierr);
+    int l4 = gmshModelGeoAddLine(p4, p1, 4, &ierr);
+    //int l5 = gmshModelGeoAddLine(p5, p6, 5, &ierr);
+    //int l6 = gmshModelGeoAddLine(p6, p7, 6, &ierr);
+    //int l6 = gmshModelGeoAddCircleArc(p7, p9, p6, 6, 0., 0., 0., &ierr); // NB : the direction of the curve is reversed
+    //int l7 = gmshModelGeoAddLine(p7, p8, 7, &ierr);
+    //int l8 = gmshModelGeoAddLine(p8, p1, 8, &ierr);
 
-    int lTags[] = {l1, l2, l3, l4, l5, -l6, l7, l8}; // NB : "-l6" because the curve is reversed 
+    int lTags[] = {l1, l2, l3, l4}; // NB : "-l6" because the curve is reversed 
     int c1[] = {1};
-    c1[0] = gmshModelGeoAddCurveLoop(lTags, 8, 1, 0, &ierr);  
+    c1[0] = gmshModelGeoAddCurveLoop(lTags, 4, 1, 0, &ierr);  
     int s1 = gmshModelGeoAddPlaneSurface(c1, 1, 1, &ierr);
     gmshModelGeoSynchronize(&ierr);
 
@@ -229,7 +218,7 @@ void geoMeshGenerateGeo() {
     if (theGeometry->elementType == FEM_QUAD) {
         gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
         gmshOptionSetNumber("Mesh.RecombineAll",1,&ierr);
-        gmshOptionSetNumber("Mesh.Algorithm",8,&ierr);  
+        gmshOptionSetNumber("Mesh.Algorithm",4,&ierr);  
         gmshOptionSetNumber("Mesh.RecombinationAlgorithm",1.0,&ierr); 
         gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  
         gmshModelMeshGenerate(2,&ierr);  }
@@ -240,28 +229,49 @@ void geoMeshGenerateGeo() {
 
     gmshFltkRun(&ierr);
 }
-
-/*
-
-void geoMeshGenerate() {
+void geoMeshGenerateProjecteur() {
 
     femGeo* theGeometry = geoGetGeometry();
-
-    double w = theGeometry->LxPlate;
-    double h = theGeometry->LyPlate;
-    
+    //geoSetSizeCallback(geoSize); 
+   
     int ierr;
-    double r = w/4;
-    int idRect = gmshModelOccAddRectangle(0.0,0.0,0.0,w,h,-1,0.0,&ierr); 
-    int idDisk = gmshModelOccAddDisk(w/2.0,h/2.0,0.0,r,r,-1,NULL,0,NULL,0,&ierr); 
-    int idSlit = gmshModelOccAddRectangle(w/2.0,h/2.0-r,0.0,w,2.0*r,-1,0.0,&ierr); 
-    int rect[] = {2,idRect};
-    int disk[] = {2,idDisk};
-    int slit[] = {2,idSlit};
+    double l = theGeometry->LxPlate;  // w=1
+    double h = theGeometry->LyPlate; // h=1
+    double lc = theGeometry->h;     //0.05
 
-    gmshModelOccCut(rect,2,disk,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    gmshModelOccCut(rect,2,slit,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
-    gmshModelOccSynchronize(&ierr); 
+    int p1 = gmshModelGeoAddPoint(-2.5*l, 0, 0., lc, 1, &ierr);
+    int p2 = gmshModelGeoAddPoint( -2.5*l, h, 0., lc, 2, &ierr);
+    //int p3 = gmshModelGeoAddPoint( -0.5*l ,  h, 0., lc, 3, &ierr);
+    //int p4 = gmshModelGeoAddPoint(-0.5*l,  3*h, 0., lc, 4, &ierr);
+    //int p5 = gmshModelGeoAddPoint(0.5*l,    3*h, 0., lc, 5, &ierr);
+    //int p6 = gmshModelGeoAddPoint(0.5*l,      h, 0., lc, 6, &ierr);
+    int p7 = gmshModelGeoAddPoint(2.5*l,     h, 0., lc, 7, &ierr);
+    int p8 = gmshModelGeoAddPoint(2.5*l,   0, 0., lc, 8, &ierr);
+    //int p9 = gmshModelGeoAddPoint(5*l,   h/2 , 0., lc, 9, &ierr);
+
+    int l1 = gmshModelGeoAddLine(p1, p2, 1, &ierr);
+    int l2 = gmshModelGeoAddLine(p2, p7, 2, &ierr);
+    int l3 = gmshModelGeoAddLine(p7, p8, 3, &ierr);
+    int l4 = gmshModelGeoAddLine(p8, p1, 4, &ierr);
+    /*
+    int l5 = gmshModelGeoAddLine(p5, p6, 5, &ierr);
+    int l6 = gmshModelGeoAddLine(p6, p7, 6, &ierr);
+    int l7 = gmshModelGeoAddLine(p7, p8, 7, &ierr);
+    //int l7 = gmshModelGeoAddCircleArc(p7, p9, p8, 6, 0., 0., 0., &ierr); // NB : the direction of the curve is reversed
+    int l8 = gmshModelGeoAddLine(p8, p1, 8, &ierr);
+
+    int l9 = gmshModelGeoAddLine(p3, p6, 9, &ierr);
+    */
+    int lTags[] = {l1, l2, l3, l4};  
+    int c1[] = {1};
+//    c1[0] = gmshModelGeoAddCurveLoop(lTags, 8, 1, 0, &ierr);  
+    int s1 = gmshModelGeoAddPlaneSurface(c1, 1, 1, &ierr);
+
+    gmshModelGeoSynchronize(&ierr);
+/*
+    gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
+    gmshModelMeshGenerate(2,&ierr);
+*/
 
     if (theGeometry->elementType == FEM_QUAD) {
         gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
@@ -274,10 +284,14 @@ void geoMeshGenerate() {
     if (theGeometry->elementType == FEM_TRIANGLE) {
         gmshOptionSetNumber("Mesh.SaveAll",1,&ierr);
         gmshModelMeshGenerate(2,&ierr);  }
+
     gmshFltkRun(&ierr);
-    return;
 }
-*/
+
+
+
+
+
 
 double *femElasticitySolve(femProblem *theProblem)
 {
